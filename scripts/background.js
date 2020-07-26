1ee109c39bf6be09b25f5f6b7e28beddca1e88e0
leokodish <lkkodish@gmail.com>
2020-07-26 01:25:50 -1000

    show page action for star advertiser paid articles only

--- scripts/background.js
+++ scripts/background.js
@@ -5,7 +5,8 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
+          pageUrl: {hostEquals: 'www.staradvertiser.com'},
+          css: [".paywall-subscribe"]
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
